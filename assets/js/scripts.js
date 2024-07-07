b8bd52a74b0ed2b6f9f63bc98e0ad4459d057a64
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-07-07 10:59:10 +0000

    set up function structure for game

---
+++ assets/js/scripts.js
@@ -0,0 +1,43 @@
+function runGame() {
+
+}
+
+
+function checkAnswer() {
+
+}
+
+
+function calculateAnswer() {
+
+}
+
+
+function incrementAnswer() {
+
+}
+
+
+function incrementWrongAnswer() {
+
+}
+
+
+function displayAdditionQuestion() {
+
+}
+
+
+function displaySubtractQuestion() {
+
+}
+
+
+function displayMultiplyQuestion() {
+
+}
+
+
+function displayDivideQuestion() {
+    
+}
