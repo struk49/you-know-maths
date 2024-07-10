964ee376b1c0a069fe0d506a79dad248be1baa39
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-07-10 01:22:38 +0000

    added question for divide

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -44,8 +44,11 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract" ) {
         displaySubtractQuestion(num1, num2);
-    } else if(gameType === "divide") {
-      displayDivideQuestion(num1, num2);
+    } else if (gameType === "divide"){
+	    displayDivisionQuestion(num1,num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
     }
 
 }
@@ -90,8 +93,11 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else if (operator === "/") {
-       return [operand1 /= operand2, "divide"];
+    } else if (operator ==="/") {
+		return [operand1 / operand2, "divide"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
     }
 
 }
@@ -140,8 +146,9 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
-function displayDivideQuestion(operand1, operand2) {
-    document.getElementById(operand1).textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById(operand2).textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById(operator).textContent = "/";
+function displayDivisionQuestion(operand1, operand2) {
+        operand1 = operand1 * operand2;
+        document.getElementById("operand1").textContent = operand1;
+        document.getElementById("operand2").textContent = operand2;
+        document.getElementById("operator").textContent = "/";
 }
